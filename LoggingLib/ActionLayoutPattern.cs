9d0c26ed02ee8fbfe9044f0559201dd413d1a27f
Timo Körner <timo.koerner@bgt-germany.de>
2016-04-14 13:41:32 +0200

    missing files added

---
+++ LoggingLib/ActionLayoutPattern.cs
@@ -0,0 +1,12 @@
+﻿using log4net.Layout;
+
+namespace LoggingLib
+{
+    public class ActionLayoutPattern : PatternLayout
+    {
+        public ActionLayoutPattern()
+        {
+            AddConverter("zombieLoggingInfo", typeof (ActionConverter));
+        }
+    }
+}
---
+++ MicroservicesTest_2_HumanService/Logging/MethodCallLogging.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Castle.DynamicProxy;
+using log4net;
+using LoggingLib;
+using MicroservicesTest_2_HumanService.Models;
+
+namespace MicroservicesTest_2_HumanService.Logging
+{
+    public class MethodCallLogging:GenericLoggingInterceptor
+    {
+        public MethodCallLogging(ILog logger) : base(logger)
+        {
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            ZombieLoggingInfo info = new ZombieLoggingInfo();
+            info.Class = invocation.TargetType.FullName;
+            info.MethodName = invocation.Method.Name + " <S>";
+            info.ZombieItSelf = GetZombieSightingFromArguments(invocation);
+
+            var sw = Stopwatch.StartNew();
+            Logger.Debug(info);
+
+            invocation.Proceed();
+            sw.Stop();
+            info.Duration = sw.Elapsed.Milliseconds;
+            info.MethodName = invocation.Method.Name + " <F>";
+            Logger.Debug(info);
+        }
+
+        public string GetZombieSightingFromArguments(IInvocation invocation)
+        {
+            if (invocation.Arguments != null)
+            {
+                return (from arg in invocation.Arguments where arg != null && arg.GetType() == typeof (ZombieSighting) select ((ZombieSighting) arg).ToString()).FirstOrDefault();
+            }
+
+            return "";
+        }
+    }
+}
